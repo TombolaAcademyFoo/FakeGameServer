0cbdca26040f155764e2b9d0ea9956612477f8b6
Andrew Mollett <Andrew.Mollett@tombola.com>
2016-01-04 16:46:24 +0000

    Added mysql to the node modules so that database connectivity is possible. Added connection functionality such that the database can be accessed by the API. Need to now use this connection in order to allow for users to use any details they have the credentials for, at current only a single hard coded user can log in.

--- bingo-server.js
+++ bingo-server.js
@@ -6,6 +6,13 @@
         userLoginMiddleware = require('./express-middleware/user-login-middleware'),
         gameMiddleware = require('./express-middleware/game-middleware'),
         cors = require('./express-middleware/cors'),
+        mysql = require('mysql'),
+        connection = mysql.createConnection({
+            host: process.env.taApiDBHost,
+            user: process.env.taApiDBUser,
+            password: process.env.taApiDBPassword,
+            database: process.env.taApiDBName
+        }),
         app = express();
 
     app.use(bodyParser.json());
@@ -13,6 +20,7 @@
     logging(app);
     userLoginMiddleware(app);
     gameMiddleware(app);
+    connection.connect();
 
     app.listen(30069, function(){
         console.log("express-winston demo listening on port %d in %s mode", this.address().port, app.settings.env);
